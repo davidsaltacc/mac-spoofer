bdb70927a6b5cc462965e50c8c43a8090768716a
davidsaltacc <d.lumpi@outlook.de>
2025-03-12 21:35:16 +0100

    add icon

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
 go install github.com/akavel/rsrc@latest
-rsrc -manifest main.manifest -o rsrc.syso
+rsrc -manifest main.manifest -o rsrc.syso -arch amd64 -ico icon.ico
 go build -ldflags="-H windowsgui"
+rm rsrc.syso
